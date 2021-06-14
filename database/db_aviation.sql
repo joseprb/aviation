a9a091f64027dd1d967521f5b64de77e19b847ed
Jose Yolanda Purba <josprb@gmail.com>
2021-06-14 08:46:13 +0700

    update

--- database/db_aviation.sql
+++ database/db_aviation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2021 at 03:37 AM
+-- Generation Time: Jun 14, 2021 at 03:43 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.3.27
 
